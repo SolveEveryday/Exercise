bdc9aa335f90e160bb25403989ce7246fe3b79a1
sue4869 <sue4869@naver.com>
2022-10-03 20:59:53 +0900

    Day1 문제풀이

---
+++ Study01 - Implementation/Day01/ysm.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> output(2);
+        for(int x = 0; x < nums.size()-1; x++) {
+            for(int y = x+1; y < nums.size(); y++) {
+            int num1 = nums[x];
+            int num2 = nums[y];
+            
+                if(num1 + num2 == target) {
+                    output[0] = x;
+                    output[1] = y;
+                    break;
+                }
+            }
+        }
+        
+        return output;
+    }
+};
