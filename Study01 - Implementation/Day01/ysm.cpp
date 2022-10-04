826f2f986dea7a50f849a7ec921a02928bc3038f
sue4869 <sue4869@naver.com>
2022-10-04 20:22:49 +0900

    refactor: 줄바꿈

--- Study01 - Implementation/Day01/ysm.cpp
+++ Study01 - Implementation/Day01/ysm.cpp
@@ -4,8 +4,8 @@ public:
         vector<int> output(2);
         for(int x = 0; x < nums.size()-1; x++) {
             for(int y = x+1; y < nums.size(); y++) {
-            int num1 = nums[x];
-            int num2 = nums[y];
+                int num1 = nums[x];
+                int num2 = nums[y];
             
                 if(num1 + num2 == target) {
                     output[0] = x;
