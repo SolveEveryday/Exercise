aa37170a9f86be2e8784f4be06e6ee2b3f99bfd9
sue4869 <sue4869@naver.com>
2022-10-12 23:16:02 +0900

    유수민 10일차 문제 풀이

---
+++ Study01 - Implementation/Day10/ysm.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+    char input[51];
+    char alphabat[26] = { 0 };
+    int middle = -1;
+    int cnt = 0;
+    int flag = 0;
+    string sen, tmp;
+    
+    cin >> input;
+    int len = strlen(input);
+    
+    for (int x = 0; x < len; x++) {
+        alphabat[input[x] - 'A']++;
+    }
+    
+    if (len % 2 == 0) {
+        for (int x = 0; x < 26; x++) {
+            if (alphabat[x] % 2 != 0) {
+                flag = 1;
+                break;
+            }
+        }
+    }
+    else {
+        for (int x = 0; x < 26; x++) {
+            if (alphabat[x] % 2 != 0) {
+                cnt++;
+                alphabat[x]--;
+                middle = x;
+            }
+        }
+
+        if (cnt > 1) flag = 1;
+    }
+
+    if (flag == 1) {
+        cout << "I'm Sorry Hansoo";
+    }
+    else {
+        for (int y = 0; y < 26; y++) {
+            if (alphabat[y] != 0) {
+                for (int x = 0; x < alphabat[y] / 2; x++) {
+                    sen += 'A' + y;
+                }
+            }
+        }
+
+        tmp = sen;
+        reverse(tmp.begin(), tmp.end());
+        if (middle != -1) sen += 'A' + middle;
+        sen += tmp;
+
+        cout << sen;
+    }
+
+    return 0;
+}
+
+
