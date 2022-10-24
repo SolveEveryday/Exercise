3be0581490e79fff454e49c166a19f3cc6860915
deepredk <deepred.kim@gmail.com>
2022-10-24 18:01:36 +0900

    solve: 22일차 문제 풀이

---
+++ Study02 - Sort/Day22/kjh.kt
@@ -0,0 +1,41 @@
+fun main() {
+    val N = readln().toInt()
+    
+    val meetings = ArrayList<Meeting>()    
+    repeat(N) {
+        val (start, end) = readln().split(" ").map { it.toInt() }
+        val meeting = Meeting(start, end)
+        meetings.add(meeting)
+    }
+    
+    val sortedMeetings = quickSort(meetings)
+    var currentEnd = -1
+    var meetingCount = 0
+    for (meeting in sortedMeetings) {
+        if (meeting.start >= currentEnd) {
+            currentEnd = meeting.end
+            meetingCount++
+        }
+    }
+    
+    print(meetingCount)
+}
+
+fun quickSort(items: List<Meeting>): List<Meeting> {
+    if (items.size < 2) {
+        return items
+    }
+
+    val pivot = items[items.size / 2]
+    val left = items.filter { it < pivot }
+    val right = items.filter { it > pivot }
+
+    return quickSort(left) + listOf(pivot) + quickSort(right)
+}
+
+data class Meeting(val start: Int, val end: Int) {
+    operator fun compareTo(other: Meeting) = when {
+        end != other.end -> end - other.end
+        else -> start - other.start
+    }
+}
