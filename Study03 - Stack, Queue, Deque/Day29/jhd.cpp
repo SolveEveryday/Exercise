e498f3b38c8751e64af136d8a85c4cf2b547a38a
jhd7130 <jhd7130@github.com>
2022-11-11 15:30:31 +0900

    Delete .vscode File

--- Study03 - Stack, Queue, Deque/Day29/jhd.cpp
+++ Study03 - Stack, Queue, Deque/Day29/jhd.cpp
@@ -4,7 +4,14 @@ string arr;
 vector<char> result;
 int main() {
     cin >> arr;
-   
+    result.push_back(arr[0]);
+    for (int i = 1; i < arr.size(); i++) {
+        if (result.back() == '(' && arr[i] == ')') {
+        result.pop_back();
+        }else {
+        result.push_back(arr[i]);
+    }
+    }
    for (char a : arr) {
     if(result.empty()){
         result.push_back(a);
