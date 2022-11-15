b046c131b551db8be25211f1d9fabbb47d23c8c9
sue4869 <sue4869@naver.com>
2022-11-16 00:08:54 +0900

    34번 문제 더 쉬운 풀이

--- Study03 - Stack, Queue, Deque/Day34/ysm.cpp
+++ Study03 - Stack, Queue, Deque/Day34/ysm.cpp
@@ -1,72 +1,58 @@
 #include <string>
 #include <vector>
-#include <iostream>
- 
+#include <queue>
 using namespace std;
- 
+
 int solution(vector<int> queue1, vector<int> queue2) {
- 
-    //선언
-    int answer;                         //-> 정답
-    vector<int>::iterator ptr1, ptr2;   //-> 큐 포인터
-    long long sum_all, sum1, sum2;      //-> 큐1+큐2합, 큐1합, 큐2합
- 
-    //초기화
-    answer = 0;
-    ptr1 = queue1.begin(), ptr2 = queue2.begin();
-    sum_all = 0, sum1 = 0, sum2 = 0;
- 
-    /* 해법 */
-    //1. 큐1, 큐2, 큐1+큐2 원소 합 구하기
-    for (int i = 0; i < queue1.size(); i++) {
-        sum1 += queue1[i];
-        sum2 += queue2[i];
+    long long sum = 0, sum2 = 0;
+    queue<int> q, q2;
+
+    for (int num : queue1) {
+        sum += num;
+
+        q.push(num);
     }
-    sum_all = sum1 + sum2;
- 
-    //2. 큐1+큐2 원소 합이 홀수일 경우 -> 불가능
-    if (sum_all % 2 != 0) {
-        answer = -1;
+
+    for (int num : queue2) {
+        sum2 += num;
+
+        q2.push(num);
     }
-    else {
-        //3. 큐1, 큐2에 있는 두 개의 포인터를 조종하며 원소 합 구하기
-        while (true) {
- 
-            //종료조건1 -> 두 큐의 합이 같을 경우
-            if (sum1 == sum2) {
-                break;
-            }
- 
-            //종료조건2 -> 포인터1이 큐2 끝까지 갔을 경우 or 포인터2가 큐1 끝까지 갔을 경우(탐색완료)
-            if (ptr1 == queue2.end() || ptr1 == queue1.end()) {
-                answer = -1;
-                break;
-            }
- 
-            //큐의 포인터 옮기기
-            if (sum1 > sum2) {
-                sum1 -= *ptr1;
-                sum2 += *ptr1;
-                
-                ptr1++;
-                if (ptr1 == queue1.end()) {
-                    ptr1 = queue2.begin();
-                }
-            }
-            else {
-                sum2 -= *ptr2;
-                sum1 += *ptr2;
-                
-                ptr2++;
-                if (ptr2 == queue2.end()) {
-                    ptr2 = queue1.begin();
-                }
-            }
- 
-            answer++;
+
+    int idx = 0, idx2 = 0;
+    int maxIdx = queue1.size() + queue2.size();
+    int answer = 0;
+
+    while (idx < maxIdx && idx2 < maxIdx)
+    {
+        if (sum == sum2)
+        {
+            return answer;
+        }
+
+        answer++;
+
+        if (sum < sum2)
+        {
+            int cur = q2.front();
+            q2.pop();
+
+            q.push(cur);
+            sum += cur;
+            sum2 -= cur;
+            idx2++;
+        }
+        else
+        {
+            int cur = q.front();
+            q.pop();
+
+            q2.push(cur);
+            sum2 += cur;
+            sum -= cur;
+            idx++;
         }
     }
- 
-    //반환
-    return answer;
+
+    return -1;
 }
