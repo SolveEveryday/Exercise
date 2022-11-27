57a8596e083f080bf5aa68f585cfe37ea3e5e34a
HanHyunSoo <gustn4563@gmail.com>
2022-11-27 20:14:06 +0900

    solve: 44일차 문제 풀이

--- Study03 - Stack, Queue, Deque/Day44/hhs.java
+++ Study03 - Stack, Queue, Deque/Day44/hhs.java
@@ -1,5 +1,3 @@
-package BOJ_19591;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,103 +10,67 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String command = br.readLine();
         StringBuilder sb = new StringBuilder();
-        Deque<String> deque = new ArrayDeque<>();
+        Deque<BigInteger> numberDeque = new ArrayDeque<>();
+        Deque<Character> operatorDeque = new ArrayDeque<>();
 
-        if (Character.isDigit(command.charAt(0))) {
-            command = "+" + command;
+        if (command.charAt(0) == '-') {
+            sb.append('-');
+            command = command.substring(1);
         }
 
         for (char ch : command.toCharArray()) {
             if (!Character.isDigit(ch) && sb.length() != 0) {
-                deque.addLast(sb.toString());
+                numberDeque.addLast(new BigInteger(sb.toString()));
+                operatorDeque.addLast(ch);
                 sb.setLength(0);
+                continue;
             }
 
             sb.append(ch);
         }
+        numberDeque.addLast(new BigInteger(sb.toString()));
 
-        deque.addLast(sb.toString());
+        while (!operatorDeque.isEmpty()) {
+            BigInteger temp = numberDeque.pollFirst();
+            BigInteger[] aValue = new BigInteger[] {temp, numberDeque.peekFirst()};
+            numberDeque.addFirst(temp);
+            temp = numberDeque.pollLast();
+            BigInteger[] bValue = new BigInteger[] {numberDeque.peekLast(), temp};
+            numberDeque.addLast(temp);
 
-        while (deque.size() > 2) {
-            String temp = deque.pollFirst();
-            String[] aExpression = new String[] {temp, deque.peekFirst()};
-            deque.addFirst(aExpression[0]);
-            temp = deque.pollLast();
-            String[] bExpression = new String[] {deque.peekLast(), temp};
-            deque.addLast(bExpression[1]);
+            BigInteger a, b;
+            Character operator;
+            if (compareOperator(operatorDeque.peekFirst(), aValue, operatorDeque.peekLast(), bValue)
+                    && operatorDeque.size() > 1) {
+                a = numberDeque.pollFirst();
+                b = numberDeque.pollFirst();
+                operator = operatorDeque.pollFirst();
 
-            String a, b;
-            if (compareOperator(aExpression, bExpression)) {
-                a = deque.pollFirst();
-                b = deque.pollFirst();
-                deque.addFirst(convert(a, b, true));
+                numberDeque.addFirst(calculator(a, b, operator));
             } else {
-                b = deque.pollLast();
-                a = deque.pollLast();
-                deque.addLast(convert(a, b, false));
+                b = numberDeque.pollLast();
+                a = numberDeque.pollLast();
+                operator = operatorDeque.pollLast();
+
+                numberDeque.addLast(calculator(a, b, operator));
             }
         }
 
-        String answer;
-        if (deque.size() == 2) {
-            answer = convert(deque.pollFirst(), deque.pollFirst(), true);
-        } else {
-            answer = deque.pollFirst();
-        }
-
-        if (answer.charAt(0) != '-') {
-            answer = answer.substring(1);
-        }
-
-        System.out.println(new BigInteger(answer));
+        System.out.println(numberDeque.poll());
     }
 
-    // a가 크면 true b가 크면 false
-    public static boolean compareOperator(String[] aExpression, String[] bExpression) {
-        char aExpressionOperator = aExpression[1].charAt(0);
-        char bExpressionOperator = bExpression[1].charAt(0);
-
-        if ((aExpressionOperator == '*' || aExpressionOperator == '/')
-                && (bExpressionOperator == '+' || bExpressionOperator == '-')) {
+    public static boolean compareOperator(Character a, BigInteger[] aValue, Character b,
+            BigInteger[] bValue) {
+        if ((a == '*' || a == '/') && (b == '+' || b == '-')) {
             return true;
-        } else if ((aExpressionOperator == '+' || aExpressionOperator == '-')
-                && (bExpressionOperator == '*' || bExpressionOperator == '/')) {
+        } else if ((a == '+' || a == '-') && (b == '*' || b == '/')) {
             return false;
         } else {
-            String aResult = convert(aExpression[0], aExpression[1], true);
+            BigInteger aResult = calculator(aValue[0], aValue[1], a);
+            BigInteger bResult = calculator(bValue[0], bValue[1], b);
 
-            if (aResult.charAt(0) != '-') {
-                aResult = aResult.substring(1);
-            }
-
-            String bResult = convert(bExpression[0], bExpression[1], false).substring(1);
-
-            return (new BigInteger(aResult).compareTo(new BigInteger(bResult))) >= 0;
+            return (aResult.compareTo(bResult)) >= 0;
         }
-    }
-
-    public static String convert(String a, String b, boolean isFirst) {
-        char leftOperator = a.charAt(0);
-        BigInteger leftOperand;
-
-        if (isFirst && leftOperator == '-') {
-            leftOperand = new BigInteger(a);
-        } else {
-            leftOperand = new BigInteger(a.substring(1));
-        }
-
-        char rightOperator = b.charAt(0);
-        BigInteger rightOperand = new BigInteger(b.substring(1));
-
-        BigInteger result = calculator(leftOperand, rightOperand, rightOperator);
-
-        String answer = result.toString();
-
-        if (result.compareTo(BigInteger.ZERO) >= 0) {
-            answer = leftOperator + answer;
-        }
-
-        return answer;
     }
 
     public static BigInteger calculator(BigInteger a, BigInteger b, char operator) {
@@ -124,4 +86,3 @@ public class Main {
         }
     }
 }
-
