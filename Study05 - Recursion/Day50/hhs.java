2d95c9c1e43856f6a15bd03b11963ecb25b49655
HanHyunSoo <gustn4563@gmail.com>
2022-12-03 18:41:32 +0900

    solve: 50일차 문제 풀이

---
+++ Study05 - Recursion/Day50/hhs.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static StringBuilder sb = new StringBuilder("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+    public static int N;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        r(0);
+        System.out.println(sb);
+    }
+
+    public static void r(int count) {
+        if (count > N) {
+            return;
+        }
+        sb.append("____".repeat(count)).append("\"재귀함수가 뭔가요?\"").append("\n");
+
+        if (count == N) {
+            sb.append("____".repeat(count)).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
+        } else {
+            sb.append("____".repeat(count))
+                    .append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.").append("\n");
+            sb.append("____".repeat(count)).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.")
+                    .append("\n");
+            sb.append("____".repeat(count))
+                    .append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append("\n");
+        }
+        r(count + 1);
+        sb.append("____".repeat(count)).append("라고 답변하였지.").append("\n");
+    }
+}
