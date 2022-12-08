32cadb05878a13c72980b22197213f1764974d10
HyunSoo Han <37373826+HanHyunsoo@users.noreply.github.com>
2022-12-08 13:33:35 +0900

    fix: 문제 수정

--- Study05 - Recursion/Day52/hhs.java
+++ Study05 - Recursion/Day52/hhs.java
@@ -5,35 +5,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
 public class Main {
-    static int[][] matrix;
-    static int N, r, c;
-    static int count = 0;
+    static int N, R, C, count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-
-        int powValue = (int) Math.pow(2, N);
-        matrix = new int[powValue][powValue];
-        R(0, 0, powValue - 1, powValue - 1);
-
-        System.out.println(matrix[r][c]);
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        solve((int) Math.pow(2, N), R, C);
     }
 
-    public static void R(int rS, int cS, int rE, int cE) {
-        if (rE - rS == 1 && cE - cS == 1) {
-            matrix[rS][cS] = count++;
+    public static void solve(int size, int r, int c) {
+        if (size == 1) {
+            System.out.println(count);
             return;
         }
 
-        R(rS, cS, rE / 2, cE / 2);
-        R(rS, (cE / 2) + 1, rE / 2, cE);
-        R((rE / 2) + 1, cS, rE, cE / 2);
-        R((rE / 2) + 1, (cE / 2) + 1, rE, cE);
+        int n = size / 2;
+        if (r < n && c < n) { // 1사분면
+            count += n * n * 0;
+            solve(n, r, c);
+        } else if (r < n && c < n + n) { // 2사분면
+            count += n * n * 1;
+            solve(n, r, c - n);
+        } else if (r < n + n && c < n) { // 3사분면
+            count += n * n * 2;
+            solve(n, r - n, c);
+        } else if (r < n + n && c < n + n) { // 4사분면
+            count += n * n * 3;
+            solve(n, r - n, c - n);
+        }
     }
 }
